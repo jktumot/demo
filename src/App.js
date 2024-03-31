22f93ad1f7f52bb2ba82ada57c043fbd0bb51f44
jktumot <78263235+jktumot@users.noreply.github.com>
2024-03-31 16:48:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Section from './Components/sectionOne/Section';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
